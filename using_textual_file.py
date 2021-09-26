801da9729b46f088cf4cc745b7e968446a4053f7
Raghav Agrawal <81867011+RaghavAgrawal17@users.noreply.github.com>
2021-09-26 23:34:44 +0530

    Create using_textual_file.py
    This code will take the text file as the input for wordcloud formation

---
+++ using_textual_file.py
@@ -0,0 +1,40 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import regex as re
+from nltk.corpus import stopwords
+import nltk
+nltk.download("punkt")
+from nltk.tokenize import word_tokenize
+
+
+text = open('modiSpeechJuly.txt', 'r',encoding="utf8" ).read()
+
+#Converting to lower case
+text = text.lower()
+
+#Removing Numbers
+text = re.sub(" \d+", " ", text)
+
+#Removing common stopwords
+text_tokens = word_tokenize(text)
+tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
+text = (" ").join(tokens_without_sw)
+
+#Removing punctuation
+text_tokens = word_tokenize(text)
+new_words= [word for word in text_tokens if word.isalnum()]
+text = (" ").join(new_words)
+
+#Removing extra white spaces
+text = re.sub(' +', ' ', text)
+
+wc = WordCloud(background_color = 'white', width = 1920, height = 1080)
+wc.generate_from_text(text)
+
+# plot the WordCloud image                      
+plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wc)
+plt.axis("off")
+plt.tight_layout(pad = 0)
+ 
+plt.show()
